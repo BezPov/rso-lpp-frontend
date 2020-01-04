738cf8dce1f59db1156587918175422b713014b9
Luka Bezovšek <luka.bezovsek@gmail.com>
2020-01-04 09:50:42 +0100

    Included vuetify

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-    devServer: {
-        disableHostCheck: true
-    },
-    publicPath: '/app/'
-};
+  "devServer": {
+    "disableHostCheck": true
+  },
+  "publicPath": "/app/",
+  "transpileDependencies": [
+    "vuetify"
+  ]
+}
