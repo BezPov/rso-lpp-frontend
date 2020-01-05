8890f5f213cf0db50f343a486e9c9aa89e4685f4
Luka Bezovšek <luka.bezovsek@gmail.com>
2020-01-05 23:43:33 +0100

    Implemented paymen, version bump to 1.0.6

---
+++ src/components/generic/FeedbackModal.vue
@@ -0,0 +1,48 @@
+
+<template>
+	<v-row justify="center">
+
+		<v-dialog
+				v-model="dialog"
+				max-width="290"
+		>
+			<v-card>
+				<v-card-title class="headline">Pošljite nam povratne informacije</v-card-title>
+
+				<v-card-text>
+					Koliko je znašala zamuda avtobusa?
+				</v-card-text>
+
+				<v-card-actions>
+					<v-spacer></v-spacer>
+
+					<v-btn
+							color="red"
+							text
+							@click="dialog = false"
+					>
+						Ne pošlji
+					</v-btn>
+
+					<v-btn
+							color="#33C88F"
+							text
+							@click="dialog = false"
+					>
+						Pošlji
+					</v-btn>
+				</v-card-actions>
+			</v-card>
+		</v-dialog>
+	</v-row>
+</template>
+
+<script>
+	export default {
+		name: "FeedbackModal"
+	}
+</script>
+
+<style scoped>
+
+</style>
